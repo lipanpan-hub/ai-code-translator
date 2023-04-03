f3472078ce84532531efa5101111d58d7a7baf39
lipanpan-hub <47287977+lipanpan-hub@users.noreply.github.com>
2023-04-03 21:22:41 +0800

    更新4

--- pages/index.tsx
+++ pages/index.tsx
@@ -149,7 +149,7 @@ export default function Home() {
             href="https://syno.lipanpanx.com:65473/"
           > 
             <button
-              className="mt-1 h-[24px] w-[280px] rounded-md border border-gray-300 px-3 py-2 text-black shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              className="mt-1 h-[24px] w-[28px] rounded-md border border-gray-300 px-3 py-2 text-black shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
             >
               获取KEY
             </button> 
@@ -173,7 +173,7 @@ export default function Home() {
             ? 'Translating...'
             : hasTranslated
             ? 'Output copied to clipboard!'
-            : 'https://lipanpanx.com'}
+            : 'https://codet.lipanpanx.com'}
         </div>
 
         <div className="mt-6 flex w-full max-w-[1200px] flex-col justify-between sm:flex-row sm:space-x-4">
